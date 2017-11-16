0e2e40ceea0edd3d06d3d74d5e364fb6ad41ca5b
Yurii Shestakov <yuriis@mellanox.com>
2017-11-16 19:44:38 +0200

    Added installation of MLNX_OFED. VF pass-through is still hardcoded

--- centos7_mofed_vm/README.md
+++ centos7_mofed_vm/README.md
@@ -4,4 +4,13 @@ Need to note that installation of MLNX OFED requires presence of MLNX HCA,
 so that I have find out the way how to add  VF (as pci device) 
 passed-through into the VM at the start.
 That is tricky a bit: I need to know which VF is not used, i.e. could
-be added to the new VM.
+be added to the new VM. Right know it's hardcoded into Vagrantfile like this:
+
+      config.vm.define vm_name do |domain|
+        domain.vm.provider :libvirt do |libvirt|
+            libvirt.pci :bus => '0x04', :slot => '0x04', :function => '0x1'
+        end
+        ...
+      end
+
+
--- centos7_mofed_vm/provision_mofed_vm.sh
+++ centos7_mofed_vm/provision_mofed_vm.sh
@@ -1,31 +1,56 @@
 #!/bin/sh
 # Author: yuriis
 if [ -z "$MLNX_OFED_URL" ] ; then
-  MLNX_OFED_URL=http://www.mellanox.com/downloads/ofed/MLNX_EN-4.2-1.0.1.0/mlnx-en-4.2-1.0.1.0-rhel7.4-x86_64.tgz
+  # MLNX_EN_URL=http://www.mellanox.com/downloads/ofed/MLNX_EN-4.2-1.0.1.0/mlnx-en-4.2-1.0.1.0-rhel7.4-x86_64.tgz
+  MLNX_OFED_URL=http://content.mellanox.com/ofed/MLNX_OFED-4.2-1.0.0.0/MLNX_OFED_LINUX-4.2-1.0.0.0-rhel7.4-x86_64.tgz
   MLNX_OFED_TGZ=${MLNX_OFED_URL##*/}
 fi
 set -e
+########################
+# Move 'vagrant' user home
+########################
+if [ -d /home/vagrant ] ; then
+  echo "*** Move 'vagrant' user home to /var/lib/vagrant"
+  mv /home/vagrant /var/lib
+  sed -i 's#:/home/vagrant:#:/var/lib/vagrant:#' /etc/passwd
+fi
+
+########################
+# Disable IPv6 first of all for the time of installation
+########################
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+if ip r l |grep -q 'default.*dev eth1' ; then
+  ip r del default dev eth0 || true
+fi
+
+########################
+# Wget, Git, Vim, Screen
+########################
+echo "Install base tools: wget, Git, ViM, screen"
+yum -q -y install wget git vim-enhanced screen 
+
 ########################
 # EPEL
 ########################
 echo "Install EPEL repo"
 yum -q install -y \
-        https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm  || true
+       https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm  || true
+#cd /vagrant
+#if [ ! -e epel-release-latest-7.noarch.rpm ] ; then
+#  wget -q https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#fi
+#yum -q install /vagrant/epel-release-latest-7.noarch.rpm || true
 
 ########################
-# Wget, Git, Vim, Screen
+# Install more utils :)
 ########################
-echo "Install wget, Git, ViM, screen, pcituils, fio, psmisc, lsof"
-yum -q -y install wget git make gcc g++ glibc-devel vim-enhanced screen pciutils \
+echo "Install kernel-devel, pcituils, fio, psmisc, lsof, ..."
+yum -q -y install gcc g++ glibc-devel pciutils \
     fio psmic lsof createrepo rpm-build nvme-cli gtk2 atk \
+    gcc-gfortran tcsh \
     kernel-devel-`uname -r`
 
-########################
-# Postinstall
-########################
-echo "*** Move 'vagrant' user home to /var/lib/vagrant"
-mv /home/vagrant /var/lib
-sed -i 's#:/home/vagrant:#:/var/lib/vagrant:#' /etc/passwd
 
 
 ########################
@@ -40,11 +65,44 @@ test -x ../postinstall_itlab.sh && ../postinstall_itlab.sh
 # Install "latest" OFED 4.2
 # build=latest-4.2 /.autodirect/mswg/release/MLNX_OFED/mlnx_ofed_install \
 #   --with-nvmf  --add-kernel-support
+echo "** Install MLNX_OFED"
 cd /tmp
 wget -q ${MLNX_OFED_URL}
 tar zxf ${MLNX_OFED_TGZ}
 cd ${MLNX_OFED_TGZ%.tgz}
-./install --with-nvmf 
 
+# MLNX_EN:
+# yes| ./install --with-nvmf
+#   enable NICs - load modules
+# /etc/init.d/mlnx-en.d start
 
+# MLNX_OFED:
+yes | ./mlnxofedinstall --dpdk --guest --with-nvmf
+#   load kernel modules
+/etc/init.d/openibd start
+
+# need to configure ipaddress(s)
+# $ ibdev2netdev
+# mlx5_0 port 1 ==> ib0 (Up)
+mgmt_ip=$( ip a l eth1 |awk '$1 == "inet" {print $2}' )
+
+ibdev2netdev |while read p _0 n _1 i _2 ; do 
+  ifcfg=/etc/sysconfig/network-scripts/ifcfg-$i
+  ip="1$n.${mgmt_ip#10.}"
+  mask=$( ipcalc -m $ip )
+  ntw=$( ipcalc -n $ip )
+  brc=$( ipcalc -b $ip )
+  ipa=
+  cat <<EOF > $ifcfg
+DEVICE=$i
+BOOTPROTO=static
+IPADDR=${ip%/[0-9]*}
+$brc
+$mask
+$ntw
+ONBOOT=yes
+NM_CONTROLLED=no
+EOF
+  ifup $i
+done
 echo "*** Done"
