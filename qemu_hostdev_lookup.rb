2b5b9092cf5cec1ced84e6df8c865af672fab065
Yurii Shestakov <yuriis@mellanox.com>
2017-11-17 20:57:15 +0000

    Fixed qemu_hostdev_lookup: reimplemented w/o nokogiri

--- qemu_hostdev_lookup.rb
+++ qemu_hostdev_lookup.rb
@@ -1,6 +1,6 @@
-#!/opt/vagrant/embedded/bin/ruby
 #!/usr/bin/env ruby
-require 'nokogiri'
+#!/opt/vagrant/embedded/bin/ruby
+# require 'nokogiri'
 
 
 QEMU_LIBVIRT_DIR='/etc/libvirt/qemu/'
@@ -29,15 +29,17 @@ module QemuHostdevLookup
             # fn='/etc/libvirt/qemu/dev-r-vrt-079-006_yuriis.xml'
             fn = File.join(QEMU_LIBVIRT_DIR, item)
             vm_name = item[0..-5]
-            doc = Nokogiri::XML(File.open(fn))
-            doc.xpath('//hostdev/source/address').each do |dev|
-                # puts dev
-                bus = dev.attributes['bus'].value.to_i(16)
-                slot = dev.attributes['slot'].value.to_i(16)
-                func = dev.attributes['function'].value.to_i(16)
-                addr = sprintf("%02x:%02x.%x", bus, slot, func)
-                mlnx_devs[addr] = vm_name
-            end
+            File.open(fn, 'r+') { |fi|
+              fi.read.lines do |line|
+                  m = line.match(rx)
+                  next unless m 
+                  bus = m[2].to_i(16)
+                  slot = m[3].to_i(16)
+                  func = m[4].to_i(16)
+                  addr = sprintf("%02x:%02x.%x", bus, slot, func)
+                  mlnx_devs[addr] = vm_name
+              end
+            }
         end
         ret = []
         mlnx_devs.each { |addr, pciid|
